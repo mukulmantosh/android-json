60863a11d873ea6300fa8af3b45f2df00ea9ff1a
Mukul Mantosh <mukulmantosh@yahoo.com>
2016-11-10 12:25:32 +0530

    modified set_notify fix 2

--- set_notify.php
+++ set_notify.php
@@ -13,9 +13,12 @@ if(!empty($_POST)){
 			$db_username = env('db_username');
 			$db_password = env('db_password');
 			$db_collection =  env('db_collection');
+
+
+			$connect = "mongodb://".$db_username.":".$db_password.
+				"@localhost:27017";
 			
-			$connection = new MongoClient("mongodb://$db_username:$db_password
-				@localhost:27017");
+			$connection = new MongoClient($connect);
 			
 			$db= $connection->selectDB($db_name);
 			$collection = $db->selectCollection($db_collection);
