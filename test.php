2318232cf4a37e88282957064168bada157ddab4
Mukul Mantosh <mukulmantosh@yahoo.com>
2016-11-10 12:39:27 +0530

    added test.php for debug 1

--- test.php
+++ test.php
@@ -5,11 +5,11 @@
 			$db_username = env('db_username');
 			$db_password = env('db_password');
 
-			$connect = "mongodb://".$db_username.":".$db_password."@ds149567.mlab.com:49567/$db_name";
+			// $connect = "mongodb://".$db_username.":".$db_password."@ds149567.mlab.com:49567/$db_name";
 
-			$connection = new MongoClient($connect);
+			// $connection = new MongoClient($connect);
 
-			
+			echo $db_name;			
 
 				
 
